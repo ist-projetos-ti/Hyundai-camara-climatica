02b2498701953be7b2481a76493bf7aafa9dfede
Giovanni Nespoli <ggnespoli@gmail.com>
2024-05-02 10:22:30 -0300

    refact: modal component

--- src/modules/users/pages/Users/Desktop/index.tsx
+++ src/modules/users/pages/Users/Desktop/index.tsx
@@ -1,37 +1,15 @@
 import React, { useEffect, useState } from 'react';
 
-import {
-  Button,
-  Modal,
-  ModalBody,
-  ModalCloseButton,
-  ModalContent,
-  ModalHeader,
-  ModalOverlay,
-  Select,
-  useDisclosure,
-} from '@chakra-ui/react';
-import { MdAddCircleOutline } from 'react-icons/md';
-import themeDefaults from '@style/themeDefaults';
 import { useUser } from '@modules/users/hooks/Users';
 import { IUser } from '@modules/users/interfaces';
-import { SubmitHandler, useForm } from 'react-hook-form';
 import UserTable from '@modules/users/components/UserTable';
-import { Container, Form, TableContainer } from './styles';
-
-interface IFormInput {
-  hcm_code: string;
-  name: string;
-  type: 'administrator' | 'user';
-}
+import CreateUserModal from '@modules/users/components/CreateUserModal';
+import { Container, TableContainer } from './styles';
 
 const DesktopUsersPage: React.FC = () => {
-  const { isOpen, onOpen, onClose } = useDisclosure();
-  const { register, handleSubmit } = useForm<IFormInput>();
-
   const [usersData, setUsersData] = useState<IUser[]>([]);
 
-  const { GetUsers, CreateUser } = useUser();
+  const { GetUsers } = useUser();
   const { data } = GetUsers();
 
   useEffect(() => {
@@ -40,84 +18,17 @@ const DesktopUsersPage: React.FC = () => {
     }
   }, [data]);
 
-  const onSubmit: SubmitHandler<IFormInput> = async (userData) => {
-    await CreateUser(userData);
-    onClose();
-  };
-
   useEffect(() => {
     document.title = 'Users';
   }, []);
 
   return (
-    <>
-      <Modal
-        isCentered
-        isOpen={isOpen}
-        onClose={onClose}
-        motionPreset="slideInBottom"
-        size="xl"
-      >
-        <ModalOverlay />
-        <ModalContent>
-          <ModalHeader color={themeDefaults.colors.addButton}>
-            New User Registration
-          </ModalHeader>
-          <ModalCloseButton />
-          <ModalBody>
-            <Form onSubmit={handleSubmit(onSubmit)}>
-              <h3>HCM Code</h3>
-              <input {...register('hcm_code')} type="text" />
-              <h3>Name</h3>
-              <input {...register('name')} type="text" />
-              <h3>Type</h3>
-              <Select
-                borderRadius={15}
-                backgroundColor="#efefef"
-                height={54}
-                placeholder="Select option"
-                {...register('type')}
-              >
-                <option value="ADMIN">ADMIN</option>
-                <option value="DEFAULT">DEFAULT</option>
-              </Select>
-              <h3>Password</h3>
-              <h2>1234MUDAR </h2>
-              <Button
-                marginTop={5}
-                colorScheme="teal"
-                variant="outline"
-                w={160}
-                marginBottom={5}
-                type="submit"
-              >
-                Salvar
-              </Button>
-            </Form>
-          </ModalBody>
-        </ModalContent>
-      </Modal>
-      <Container>
-        <Button
-          onClick={onOpen}
-          leftIcon={<MdAddCircleOutline size={23} />}
-          colorScheme="whiteAlpha"
-          fontSize={18}
-          height={55}
-          width={300}
-          backgroundColor={themeDefaults.colors.primary}
-          variant="solid"
-          _hover={{
-            transform: 'scale(1.1)',
-          }}
-        >
-          New user
-        </Button>
-        <TableContainer>
-          <UserTable data={usersData} />
-        </TableContainer>
-      </Container>
-    </>
+    <Container>
+      <CreateUserModal />
+      <TableContainer>
+        <UserTable data={usersData} />
+      </TableContainer>
+    </Container>
   );
 };
 
--- src/modules/users/pages/Users/Desktop/styles.ts
+++ src/modules/users/pages/Users/Desktop/styles.ts
@@ -13,30 +13,3 @@ export const TableContainer = styled.div`
   height: 100%;
   margin-top: 50px;
 `;
-
-export const Form = styled.form`
-  h3 {
-    color: ${({ theme }) => theme.colors.primary};
-    font-weight: bold;
-
-    margin-left: 2rem;
-    margin-top: 25px;
-  }
-
-  input {
-    width: 100%;
-    height: 54px;
-    background-color: #efefef;
-
-    padding-left: 2rem;
-
-    border-radius: 15px;
-  }
-
-  h2 {
-    height: 54px;
-    display: flex;
-    align-items: center;
-    margin-left: 2rem;
-  }
-`;
