26fca76680be648f94dfca590bb0f844d21435d6
gabrielamonaro <gabrielamonaro@outlook.com>
2024-06-13 15:05:13 -0300

    feat: filter time input

---
+++ src/components/Filter/TimeInput/index.tsx
@@ -0,0 +1,269 @@
+/* eslint-disable import/no-unresolved */
+import React, { useCallback, useState } from 'react';
+import { useForm } from 'react-hook-form';
+import { FormControl } from '@chakra-ui/react';
+// import Input from '@components/Form/Input';
+import { PiClockCountdown } from 'react-icons/pi';
+
+import {
+  Container,
+  DateSelector,
+  SubmitButton,
+  DateInputBox,
+  DateDivider,
+  InputGroup,
+  Form,
+  InputLabel,
+  DateLabel,
+  Button,
+  InputBundle,
+} from './styles';
+import { TimeFilterData, timeFilterResolver } from './timeFilter.zod';
+import Input from './Input';
+
+const TimeInput: React.FC = () => {
+  const [selectedItem, setSelectedItem] = useState(true);
+  const [initialDate, setInitialDate] = useState<string | null>(null);
+  const [finalDate, setFinalDate] = useState<string | null>(null);
+
+  const [showInitialDateBox, setShowInitialDateBox] = useState(false);
+  const [showFinalDateBox, setShowFinalDateBox] = useState(false);
+
+  const {
+    handleSubmit: handleInitialDateSubmit,
+    register: initialDateRegister,
+    formState: { errors: initialDateErrors },
+  } = useForm<TimeFilterData>({
+    resolver: timeFilterResolver,
+    mode: 'all',
+  });
+
+  const {
+    handleSubmit: handleFinalDateSubmit,
+    register: finalDateRegister,
+    formState: { errors: finalDateErrors },
+  } = useForm<TimeFilterData>({
+    resolver: timeFilterResolver,
+    mode: 'all',
+  });
+
+  const onInitialDateSubmit = useCallback(
+    async (data: TimeFilterData) => {
+      setInitialDate(
+        `${data.hour_h1}${data.hour_h2}:${data.minute_m1}${data.minute_m2}:${data.second_s1}${data.second_s2}`
+      );
+      if (JSON.stringify(initialDateErrors) === '{}')
+        setShowInitialDateBox(false);
+    },
+    [initialDateErrors]
+  );
+
+  const onFinalDateSubmit = useCallback(
+    async (data: TimeFilterData) => {
+      setFinalDate(
+        `${data.hour_h1}${data.hour_h2}:${data.minute_m1}${data.minute_m2}:${data.second_s1}${data.second_s2}`
+      );
+
+      if (JSON.stringify(finalDateErrors) === '{}') setShowFinalDateBox(false);
+    },
+    [finalDateErrors]
+  );
+
+  return (
+    <Container>
+      <DateSelector
+        selected={selectedItem}
+        onClick={() => setSelectedItem(true)}
+        filledDate={!!initialDate}
+      >
+        <Button
+          onClick={() => {
+            setShowInitialDateBox(!showInitialDateBox);
+            setShowFinalDateBox(false);
+          }}
+        >
+          <PiClockCountdown size={19} />
+          {initialDate ? <DateLabel> {initialDate}</DateLabel> : <p>Start</p>}
+        </Button>
+        <DateInputBox selected={showInitialDateBox}>
+          <Form onSubmit={handleInitialDateSubmit(onInitialDateSubmit)}>
+            <InputGroup>
+              <InputLabel>Hour</InputLabel>
+              <InputBundle>
+                <FormControl isInvalid={!!initialDateErrors.hour_h1}>
+                  <Input
+                    register={initialDateRegister}
+                    name="hour_h1"
+                    type="text"
+                    maxLength={1}
+                    errors={initialDateErrors.hour_h1}
+                  />
+                </FormControl>
+
+                <FormControl isInvalid={!!initialDateErrors.hour_h2}>
+                  <Input
+                    register={initialDateRegister}
+                    name="hour_h2"
+                    type="text"
+                    maxLength={1}
+                    errors={initialDateErrors.hour_h2}
+                  />
+                </FormControl>
+              </InputBundle>
+            </InputGroup>
+            <DateDivider>:</DateDivider>
+            <InputGroup>
+              <InputLabel>Minute</InputLabel>
+              <InputBundle>
+                <FormControl isInvalid={!!initialDateErrors.minute_m1}>
+                  <Input
+                    name="minute_m1"
+                    type="text"
+                    maxLength={1}
+                    register={initialDateRegister}
+                    errors={initialDateErrors.minute_m1}
+                  />
+                </FormControl>
+                <FormControl isInvalid={!!initialDateErrors.minute_m2}>
+                  <Input
+                    name="minute_m2"
+                    type="text"
+                    maxLength={1}
+                    register={initialDateRegister}
+                    errors={initialDateErrors.minute_m2}
+                  />
+                </FormControl>
+              </InputBundle>
+            </InputGroup>
+            <DateDivider>:</DateDivider>
+
+            <InputGroup>
+              <InputLabel>Second</InputLabel>
+              <InputBundle>
+                <FormControl isInvalid={!!initialDateErrors.second_s1}>
+                  <Input
+                    name="second_s1"
+                    type="text"
+                    maxLength={1}
+                    register={initialDateRegister}
+                    errors={initialDateErrors.second_s1}
+                  />
+                </FormControl>
+
+                <FormControl isInvalid={!!initialDateErrors.second_s2}>
+                  <Input
+                    name="second_s2"
+                    type="text"
+                    maxLength={1}
+                    register={initialDateRegister}
+                    errors={initialDateErrors.second_s2}
+                  />
+                </FormControl>
+              </InputBundle>
+            </InputGroup>
+            <SubmitButton type="submit">Ok →</SubmitButton>
+          </Form>
+        </DateInputBox>
+      </DateSelector>
+
+      {initialDate && finalDate && <DateDivider>{'>'}</DateDivider>}
+
+      <DateSelector
+        selected={!selectedItem}
+        onClick={() => setSelectedItem(false)}
+        filledDate={!!finalDate}
+      >
+        <Button
+          onClick={() => {
+            setShowFinalDateBox(!showFinalDateBox);
+            setShowInitialDateBox(false);
+          }}
+        >
+          <PiClockCountdown size={19} />
+          {finalDate ? <DateLabel> {finalDate}</DateLabel> : <p>End</p>}
+        </Button>
+
+        <DateInputBox selected={showFinalDateBox}>
+          <Form onSubmit={handleFinalDateSubmit(onFinalDateSubmit)}>
+            <InputGroup>
+              <InputLabel>Hour</InputLabel>
+              <InputBundle>
+                <FormControl isInvalid={!!finalDateErrors.hour_h1}>
+                  <Input
+                    register={finalDateRegister}
+                    name="hour_h1"
+                    type="text"
+                    maxLength={1}
+                    errors={finalDateErrors.hour_h1}
+                  />
+                </FormControl>
+                <FormControl isInvalid={!!finalDateErrors.hour_h1}>
+                  <Input
+                    register={finalDateRegister}
+                    name="hour_h2"
+                    type="text"
+                    maxLength={1}
+                    errors={finalDateErrors.hour_h2}
+                  />
+                </FormControl>
+              </InputBundle>
+            </InputGroup>
+
+            <DateDivider>:</DateDivider>
+            <InputGroup>
+              <InputLabel>Minute</InputLabel>
+              <InputBundle>
+                <FormControl isInvalid={!!finalDateErrors.minute_m1}>
+                  <Input
+                    name="minute_m1"
+                    type="text"
+                    maxLength={1}
+                    register={finalDateRegister}
+                    errors={finalDateErrors.minute_m1}
+                  />
+                </FormControl>
+                <FormControl isInvalid={!!finalDateErrors.minute_m1}>
+                  <Input
+                    name="minute_m2"
+                    type="text"
+                    maxLength={1}
+                    register={finalDateRegister}
+                    errors={finalDateErrors.minute_m1}
+                  />
+                </FormControl>
+              </InputBundle>
+            </InputGroup>
+            <DateDivider>:</DateDivider>
+
+            <InputGroup>
+              <InputLabel>Second</InputLabel>
+              <InputBundle>
+                <FormControl isInvalid={!!finalDateErrors.second_s1}>
+                  <Input
+                    name="second_s1"
+                    type="text"
+                    maxLength={1}
+                    register={finalDateRegister}
+                    errors={finalDateErrors.second_s1}
+                  />
+                </FormControl>
+                <FormControl isInvalid={!!finalDateErrors.second_s2}>
+                  <Input
+                    name="second_s2"
+                    type="text"
+                    maxLength={1}
+                    register={finalDateRegister}
+                    errors={finalDateErrors.second_s2}
+                  />
+                </FormControl>
+              </InputBundle>
+            </InputGroup>
+            <SubmitButton type="submit">Ok →</SubmitButton>
+          </Form>
+        </DateInputBox>
+      </DateSelector>
+    </Container>
+  );
+};
+
+export default TimeInput;
---
+++ src/components/Filter/TimeInput/styles.ts
@@ -0,0 +1,161 @@
+import styled, { css } from 'styled-components';
+
+interface IDateSelectorProps {
+  selected?: boolean;
+  filledDate?: boolean;
+}
+
+interface IDateInputContainerProps {
+  selected?: boolean;
+}
+
+interface InputProps {
+  variety?: 'year' | 'default';
+  isInvalid?: boolean;
+}
+
+export const Container = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  background-color: #efefef;
+  width: fit-content;
+  border-radius: 9px;
+`;
+
+export const DateSelector = styled.span<IDateSelectorProps>`
+  background-color: transparent;
+  width: fit-content;
+  min-width: 86px;
+  display: flex;
+  border-radius: 9px;
+  margin: 5px;
+  padding: 5px;
+  height: 31px;
+  transition: 200ms;
+  align-items: center;
+  justify-content: center;
+
+  position: relative;
+
+  :nth-child(2n-1) {
+    margin-right: 0;
+  }
+
+  ${({ selected, filledDate }) =>
+    selected &&
+    !filledDate &&
+    css`
+      background-color: #0d9f64;
+      color: white;
+    `};
+
+  svg {
+    margin-right: 8px;
+  }
+`;
+
+export const DateInputBox = styled.div<IDateInputContainerProps>`
+  width: 440px;
+  height: 105px;
+  position: absolute;
+  top: 40px;
+  left: 0;
+  border-radius: 18px;
+
+  display: flex;
+  box-shadow: 3px 3px 10px rgba(0, 0, 0, 0.15);
+  ${({ selected }) =>
+    !selected &&
+    css`
+      display: none;
+    `};
+
+  color: black;
+  align-items: center;
+  justify-content: space-around;
+  color: #757575;
+  padding: 5px;
+`;
+
+export const SubmitButton = styled.button`
+  background-color: #0d9f64;
+  color: white;
+  padding: 10px;
+  border-radius: 9px;
+  width: 68px;
+  height: 34px;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  font-size: 14px;
+  margin-top: 3%;
+`;
+
+export const InputLabel = styled.p`
+  color: #c1c1c1;
+  font-size: 14px;
+  text-align: center;
+  width: 100%;
+  margin: 0;
+`;
+
+export const InputGroup = styled.div<InputProps>`
+  width: fit-content;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+`;
+
+export const DateDivider = styled.span`
+  display: flex;
+  align-items: end;
+  height: 45%;
+  color: #757575;
+  font-weight: 500;
+  font-size: 14px;
+`;
+
+export const Form = styled.form`
+  display: flex;
+  align-items: center;
+  justify-content: space-around;
+  padding: 5px;
+  width: 100%;
+  height: 100%;
+`;
+
+export const Button = styled.button`
+  background-color: transparent;
+  width: 100%;
+  display: flex;
+  border-radius: 9px;
+  margin: 5px;
+  padding: 5px;
+  height: 31px;
+  transition: 200ms;
+  align-items: center;
+  justify-content: center;
+`;
+
+export const DateLabel = styled.p`
+  color: #0d9f64;
+  font-size: 14px;
+  font-weight: 500;
+  width: 100%;
+`;
+
+export const ErrorMessage = styled.p`
+  color: red;
+  font-size: 10px;
+  position: absolute;
+  bottom: 1%;
+  right: 4%;
+`;
+
+export const InputBundle = styled.div`
+  width: 86px;
+  display: flex;
+  justify-content: center;
+`;
