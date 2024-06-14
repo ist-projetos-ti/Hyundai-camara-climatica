7218848efe7008a0683af622f4c3b61ec61cb754
GiovanniNespoli <ggnespoli@gmail.com>
2024-06-14 10:34:54 -0300

    feat: mobile page

--- src/modules/historicalAlerts/pages/historicalAlerts/index.tsx
+++ src/modules/historicalAlerts/pages/historicalAlerts/index.tsx
@@ -1,113 +1,24 @@
-/* eslint-disable react/no-array-index-key */
-/* eslint-disable arrow-body-style */
-import HistoricalAlertsTable from '@modules/historicalAlerts/components/historicalAlertsTable';
-import IHistoricalAlertsTable from '@modules/historicalAlerts/interface/IHistoricalAlertsTable';
-import { LuChevronsUpDown } from 'react-icons/lu';
-import React, { useCallback, useState } from 'react';
-import { Container, HeaderItem, TableBody, TableHeader } from './styles';
+import React, { useEffect, useMemo } from 'react';
 
-const data = [
-  {
-    equipamentId: '155489_XYZ',
-    failure: 'HFR_ERROR1_Mem',
-    location: 'XXXYZ',
-    date: new Date(
-      'Thu Jun 15 2024 20:14:22 GMT-0300 (Horário Padrão de Brasília)'
-    ),
-    time: new Date(
-      'Thu Jun 15 2024 20:14:22 GMT-0300 (Horário Padrão de Brasília)'
-    ),
-    testId: 'HFR_ERROR1_Mem',
-  },
-  {
-    equipamentId: '155489_XYZ',
-    failure: 'HFR_ERROR1_Mem',
-    location: 'XXXYZ',
-    date: new Date(
-      'Thu Jun 20 2024 00:14:22 GMT-0300 (Horário Padrão de Brasília)'
-    ),
-    time: new Date(
-      'Thu Jun 20 2024 00:14:22 GMT-0300 (Horário Padrão de Brasília)'
-    ),
-    testId: 'HFR_ERROR1_Mem',
-  },
-  {
-    equipamentId: '155489_XYZ',
-    failure: 'HFR_ERROR1_Mem',
-    location: 'XXXYZ',
-    date: new Date(
-      'Thu Jun 15 2024 10:00:22 GMT-0300 (Horário Padrão de Brasília)'
-    ),
-    time: new Date(
-      'Thu Jun 15 2024 10:00:22 GMT-0300 (Horário Padrão de Brasília)'
-    ),
-    testId: 'HFR_ERROR1_Mem',
-  },
-];
+// eslint-disable-next-line import/no-unresolved
 
-const HistoricalAlerts: React.FC = () => {
-  const [historicalAlertsData, setHistoricalAlertData] =
-    useState<IHistoricalAlertsTable[]>(data);
+import { BREAKPOINTS } from '@style/breakpoints';
+import { Container } from './styles';
+import HistoricalAlertsDesktop from './Desktop';
+import HistoricalAlertsMobile from './Mobile';
 
-  const [sortOrderDate, setSortOrderDate] = useState<'asc' | 'desc'>('asc');
+const HistoricalAlert: React.FC = () => {
+  useEffect(() => {
+    document.title = 'HistoricalAlert';
+  }, []);
 
-  const orderByDate = useCallback(() => {
-    const sortedData = [...historicalAlertsData].sort((a, b) => {
-      if (sortOrderDate === 'asc') {
-        return a.date.getTime() - b.date.getTime();
-      }
-      return b.date.getTime() - a.date.getTime();
-    });
-    setHistoricalAlertData(sortedData);
-    setSortOrderDate(sortOrderDate === 'asc' ? 'desc' : 'asc');
-  }, [historicalAlertsData, sortOrderDate]);
-
-  const orderByTime = useCallback(() => {
-    const sortedData = [...historicalAlertsData].sort((a, b) => {
-      if (sortOrderDate === 'asc') {
-        return a.time.getTime() - b.time.getTime();
-      }
-      return b.time.getTime() - a.time.getTime();
-    });
-    setHistoricalAlertData(sortedData);
-    setSortOrderDate(sortOrderDate === 'asc' ? 'desc' : 'asc');
-  }, [historicalAlertsData, sortOrderDate]);
+  const isDesktop = useMemo(() => window.innerWidth > BREAKPOINTS.MOBILE, []);
 
   return (
     <Container>
-      <TableHeader>
-        <HeaderItem width={16.5}>
-          <p>Equipament ID</p>
-        </HeaderItem>
-        <HeaderItem width={16.5}>
-          <p>Failure</p>
-        </HeaderItem>
-        <HeaderItem width={20}>
-          <p>Location</p>
-        </HeaderItem>
-        <HeaderItem width={10}>
-          <button onClick={orderByDate}>
-            <p>Date</p> <LuChevronsUpDown />
-          </button>
-        </HeaderItem>
-        <HeaderItem width={20}>
-          <button onClick={orderByTime}>
-            <p>Time</p> <LuChevronsUpDown />
-          </button>
-        </HeaderItem>
-        <HeaderItem width={16.5}>
-          <p>Test ID</p>
-        </HeaderItem>
-      </TableHeader>
-      <TableBody>
-        {historicalAlertsData.map(
-          (value: IHistoricalAlertsTable, index: number) => {
-            return <HistoricalAlertsTable key={index} data={value} />;
-          }
-        )}
-      </TableBody>
+      {isDesktop ? <HistoricalAlertsDesktop /> : <HistoricalAlertsMobile />}
     </Container>
   );
 };
 
-export default HistoricalAlerts;
+export default HistoricalAlert;
--- src/modules/historicalAlerts/pages/historicalAlerts/styles.ts
+++ src/modules/historicalAlerts/pages/historicalAlerts/styles.ts
@@ -2,57 +2,11 @@ import styled from 'styled-components';
 
 export const Container = styled.div`
   width: 100%;
-  height: 87%;
-`;
-
-export const TableHeader = styled.div`
-  width: 100%;
-  height: 42px;
-  background-color: #efefef;
-  border-radius: 10px;
-  display: flex;
-  margin-top: 30px;
-`;
-
-export const HeaderItem = styled.h1<{ width: number }>`
   height: 100%;
-  width: ${({ width }) => width}%;
-
-  display: flex;
-  align-items: center;
-  margin-left: 2rem;
-
-  p {
-    font-size: 16px;
-    font-weight: bold;
-    color: ${({ theme }) => theme.colors.primary};
-
-    @media (max-width: 1300px) {
-      font-size: 14px;
-    }
-
-    @media (max-width: 1000px) {
-      font-size: 12px;
-    }
-  }
-
-  button {
-    display: flex;
-    align-items: center;
-    color: ${({ theme }) => theme.colors.primary};
-
-    &:hover {
-      color: #bfbfbf;
-      p {
-        color: #bfbfbf;
-      }
-    }
-  }
 `;
 
-export const TableBody = styled.div`
-  width: 100%;
-  height: 90%;
-
-  overflow: auto;
+export const Grid = styled.div`
+  height: 100vh;
+  width: 90%;
+  margin: auto;
 `;
