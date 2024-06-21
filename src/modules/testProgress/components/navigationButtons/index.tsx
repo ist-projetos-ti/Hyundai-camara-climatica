4c7987353da1d8ee07fa010b24932c467cd617aa
Giovanni Andrade Nespoli <ggnespoli@gmail.com>
2024-06-21 16:54:19 -0300

    refact: navigation

--- src/modules/testProgress/components/navigationButtons/index.tsx
+++ src/modules/testProgress/components/navigationButtons/index.tsx
@@ -1,5 +1,7 @@
 /* eslint-disable react/no-array-index-key */
 import React from 'react';
+import { PrivatePathsEnum } from '@routes/privateRoutes/privatePaths';
+import { useNavigate } from 'react-router-dom';
 import { ButtonContent, Container } from './styles';
 
 const buttons = [
@@ -23,14 +25,23 @@ const buttons = [
   },
 ];
 
-const NavigationButtons: React.FC = () => (
-  <Container>
-    {buttons.map((value, index) => (
-      <ButtonContent key={index}>
-        <h1>{value.title}</h1>
-      </ButtonContent>
-    ))}
-  </Container>
-);
+const NavigationButtons: React.FC = () => {
+  const navigate = useNavigate();
+
+  return (
+    <Container>
+      {buttons.map((value, index) => (
+        <ButtonContent
+          onClick={() => {
+            navigate(PrivatePathsEnum.NEW_TEST_PROGRESS);
+          }}
+          key={index}
+        >
+          <h1>{value.title}</h1>
+        </ButtonContent>
+      ))}
+    </Container>
+  );
+};
 
 export default NavigationButtons;
