886143244fd56b4290d5dfa28c6714475e86070c
Giovanni Andrade Nespoli <ggnespoli@gmail.com>
2024-06-21 16:57:28 -0300

    feat: test list components

--- src/modules/testProgress/pages/newTestProgress/index.tsx
+++ src/modules/testProgress/pages/newTestProgress/index.tsx
@@ -1,6 +1,7 @@
 /* eslint-disable react-hooks/rules-of-hooks */
 import React, { useEffect } from 'react';
 import NewTestProgressForm from '@modules/testProgress/components/newTestProgressForm';
+import TestsList from '@modules/testProgress/components/testsList';
 import { Container } from './styles';
 
 const NewTestProgress: React.FC = () => {
@@ -11,6 +12,7 @@ const NewTestProgress: React.FC = () => {
   return (
     <Container>
       <NewTestProgressForm />
+      <TestsList />
     </Container>
   );
 };
