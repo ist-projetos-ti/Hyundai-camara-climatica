90f1304b1b9184ec91c2b1a73ef023fd6aa5748d
Giovanni Andrade Nespoli <ggnespoli@gmail.com>
2024-06-25 16:46:00 -0300

    feat: thermocouple graph

---
+++ src/modules/testProgress/components/thermocoupleDataGraph/styles.ts
@@ -0,0 +1,37 @@
+import styled from 'styled-components';
+
+export const Container = styled.div`
+  width: 100%;
+  height: 70%;
+
+  display: flex;
+`;
+
+export const ThermocoupleGraphContent = styled.div`
+  width: 85%;
+  height: 100%;
+`;
+
+export const SliderContent = styled.div`
+  margin-top: 1rem;
+`;
+
+export const SliderInformations = styled.div`
+  width: 100%;
+  height: 50%;
+
+  display: flex;
+  justify-content: center;
+  align-items: center;
+
+  div {
+    display: flex;
+    justify-content: space-around;
+
+    width: 2.7%;
+
+    p {
+      font-size: 10px;
+    }
+  }
+`;
