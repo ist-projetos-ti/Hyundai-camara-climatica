fb41dd19a22034d562db45f7d9620f3ded592056
Giovanni Nespoli <ggnespoli@gmail.com>
2024-06-28 09:36:34 -0300

    feat: test history graph

--- src/modules/testHistory/routes/index.tsx
+++ src/modules/testHistory/routes/index.tsx
@@ -3,10 +3,15 @@ import { Route } from 'react-router-dom';
 import { PrivatePathsEnum } from '@routes/privateRoutes/privatePaths';
 import TestHistoryLayout from '../layout';
 import TestHistory from '../pages/testHistory';
+import TestHistoryGraph from '../pages/testHistoryGraph';
 
 const TestHistoryRoutes = (
   <Route element={<TestHistoryLayout />}>
     <Route path={PrivatePathsEnum.HISTORICAL_TESTS} element={<TestHistory />} />
+    <Route
+      path={PrivatePathsEnum.HISTORICAL_TESTS_GRAPH}
+      element={<TestHistoryGraph />}
+    />
   </Route>
 );
 
--- src/routes/privateRoutes/privatePaths.ts
+++ src/routes/privateRoutes/privatePaths.ts
@@ -5,6 +5,7 @@ export enum PrivatePathsEnum {
   DASHBOARD = '/dashboard',
   HISTORICAL_ALERTS = '/historical_alerts',
   HISTORICAL_TESTS = '/historical_tests',
+  HISTORICAL_TESTS_GRAPH = '/historical_tests_graph',
   TEST_PROGRESS_GRAPH = '/test_progress_graph',
   TERMOCOUPLE_DATA = '/termocoupledata',
   NEW_TEST_PROGRESS = '/new_test_progress',
